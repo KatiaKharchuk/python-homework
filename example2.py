d96d8fb696c4e0358721e2f43bf65103c787ed13
Katia <katiayamkova@gmail.com>
2021-08-02 18:31:14 +0300

    comparing value in arrays

---
+++ example2.py
@@ -0,0 +1,12 @@
+import random
+a = [random.randint(-10,10) for i in range(10)]
+print (a)
+b = [random.randint(-10,10) for i in range(10)]
+print (b)
+c = list()
+for i in range(1, 10):
+    for j in range(1, 10):
+        if a[i] == b[j]:
+            c.append(a[i])
+
+print(c)
---
+++ module.main.py
@@ -0,0 +1,8 @@
+import random
+
+number = random.randrange(10) # значение от 0 до 10
+print(number)
+number = random.randrange(2, 10)# значение в диапазоне 2, 3, 4, 5, 6, 7, 8, 9, 10
+print(number)
+number = random.randrange(2, 10, 2)# значение в диапазоне 2, 4, 6, 8, 10
+print(number)
